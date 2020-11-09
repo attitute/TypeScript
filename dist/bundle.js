1b5eb74bed1831b77b333f21f536728347690c38
hsy <18296576390@163.com>
2020-11-10 07:19:25 +0800

    接口

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,22 +1,19 @@
 (function () {
     'use strict';
 
-    // 类 es6 类来调用的静态属性 私有的实列属性 共享的原型属性
-    // 修饰符 public(公开的) private(私有的) protected(只能父类与子类使用) readonly(只读)
-    class Animal {
-        constructor(name, age) {
+    // 两个接口不能有冲突
+    // 可以描述对象 函数 类 类的实例
+    // <T>就是泛型 函数调用时传入类型 
+    class Person {
+        constructor(name) {
             this.name = name;
-            this.age = age;
+            this.name = name;
         }
     }
-    class Cat extends Animal {
-        constructor(name, age, address) {
-            super(name, age);
-            this.address = '';
-            console.log(this.name);
-        }
+    function createInstance(clazz, name) {
+        return new clazz(name);
     }
-    let cat = new Cat('tom', 10, '美国');
+    let r = createInstance(Person, 'me');
 
 }());
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,62 @@
 
-// 类 es6 类来调用的静态属性 私有的实列属性 共享的原型属性
-// public公开
-class Pointer {
-    public x!:number
-    public y!:number
-    constructor(x:number,y?:number){
-        this.x = x
-        this.y = y as number
+
+// 接口可以被类实现 把接口用在类上
+interface Speakable { // 接口中的内容都是抽象，没有具体的实现
+    name: string,
+    speak(): void // 描述类的原型方法 ，void 表示不关心方法的返回值
+}
+
+interface ChineseSpeakable {
+    speakChinese():void
+}
+
+// 两个接口不能有冲突
+class Speak implements Speakable,ChineseSpeakable {
+    speakChinese(): void {
+        throw new Error("Method not implemented.");
+    }
+    speak(): number {
+        throw new Error("Method not implemented.");
+    }
+    name!: string;
+    
+}
+
+
+// 类 抽象类 不能被实例化 只有抽象类可以标记abstract 标记abstract才需要子类实现
+abstract class Animal { // 抽象类中可以包含抽象方法和抽象属性
+    abstract name:string // 抽象没有实现 
+    eat(){ // 有实现（是否赋值）
+        console.log('eat')
     }
 }
-let pointer = new Pointer(1)
+
+class Tom extends Animal {
+    name!: string;
+    
+}
+
+// 可以描述对象 函数 类 类的实例
 
 
-// 修饰符 public(公开的) private(私有的) protected(只能父类与子类使用) readonly(只读)
-
-class Animal {
-    public name!:string
-    public readonly age!:number
-    constructor(name:string,age:number){
+// <T>就是泛型 函数调用时传入类型 
+class Person {
+    constructor(public name:string){
         this.name = name
-        this.age = age
-    }
-}
-let animal = new Animal('jk',10)
-
-class Cat extends Animal {
-    address = ''
-    constructor(name:string,age:number,address:string) {
-        super(name, age)
-        console.log(this.name)
     }
 }
 
-let cat = new Cat('tom', 10, '美国')
+interface Clazz<T> {
+    new (name:string):T
+}
+
+function createInstance<T> (clazz:Clazz<T>, name:string){
+    return new clazz(name)
+}
+
+let r = createInstance<Person>(Person, 'me')
+
+
 
 
 
